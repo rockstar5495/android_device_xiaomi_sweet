e2f54476f921684abe66996b9187671c8f54a9d9
Akash <akashniki@gmail.com>
2019-11-01 00:05:02 +0530

    sweet: Switch to minimal sepolicy
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,7 +132,8 @@ TARGET_SCREEN_DENSITY := 440
 
 # Sepolicy
 include device/qcom/sepolicy/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-minimal/private
 
 # Neverallows
 SELINUX_IGNORE_NEVERALLOWS := true
