8bd8ea162c782fb75a990ac5ef5a160ebac73358
Ali Hasan <ahb7671@gmail.com>
2021-05-12 10:32:02 +0500

    sweet: parts: import missing resources
    from https://github.com/LineageOS/android_packages_resources_devicesettings
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- parts/Android.mk
+++ parts/Android.mk
@@ -24,8 +24,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     vendor.xiaomi.hardware.touchfeature-V1.0-java
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+    $(LOCAL_PATH)/res
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     vendor.xiaomi.hardware.touchfeature-V1.0-java
