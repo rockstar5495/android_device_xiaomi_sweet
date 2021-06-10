251abb8ffea2d40e26c2a2f7d6a7fb969c339225
AdarshGrewal <adarsh@aosip.dev>
2021-06-10 20:55:12 +0530

    sweet: init: SafetyNet workaround
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -179,6 +179,8 @@ void vendor_load_properties() {
     // SafetyNet workaround
     fingerprint = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
     description = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
+    property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.boot.flash.locked", "1");
     workaround_snet_properties();
 
     set_ro_build_prop("fingerprint", fingerprint);
