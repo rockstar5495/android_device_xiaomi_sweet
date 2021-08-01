9c586ef2f91a85c6d96ec828d6f8e7635b9bd911
Zinadin Zidan <zidan.roking@gmail.com>
2021-08-01 06:54:56 +0600

    sweet: Import device-specific display configs
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I07bd5d517d7dd008f77e9e8adbd668db8049f058

--- device.mk
+++ device.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
     DevicesOverlay \
     DevicesAndroidOverlay
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
