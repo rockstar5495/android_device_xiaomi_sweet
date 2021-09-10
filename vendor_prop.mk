3b6f49206077e791ff36da0e0bf43ff38c6182ca
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-09-11 01:06:20 +0600

    sweet: prop: Relax refresh rate switching
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I39aab49f2291be2af24cc111dfa8db84e07e75ac
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -6,8 +6,8 @@ ro.vendor.dolby.dax.version=DAX3_3.6.0.12_r1
 persist.vendor.audio.voicecall.speaker.stereo=true
 
 # Display Dynamic Refresh Rate
-ro.surface_flinger.set_idle_timer_ms=80
-ro.surface_flinger.set_touch_timer_ms=200
+ro.surface_flinger.set_idle_timer_ms=500
+ro.surface_flinger.set_touch_timer_ms=750
 ro.surface_flinger.support_kernel_idle_timer=true
 ro.surface_flinger.set_display_power_timer_ms=1000
 ro.surface_flinger.use_content_detection_for_refresh_rate=true
