3bf6f8a36b184ad4fe981c4c0453e349ab5a92e8
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-09-10 14:40:12 +0000

    sweet: parts: Drop touchfeature impl of dt2w * Let xiaomi-touch handle dt2w * This commit partially reverts a001d48476000d0cfc50e8977c440f36882793da
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -19,7 +19,6 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.SystemProperties;
 import androidx.preference.PreferenceManager;
 
@@ -29,13 +28,7 @@ import org.lineageos.settings.utils.RefreshRateUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.utils.FileUtils;
 
-import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
-
-
 public class BootCompletedReceiver extends BroadcastReceiver {
-
-    public static final String SHAREDD2TW = "sharadeD2TW";
-    private ITouchFeature mTouchFeature;
 
     private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
     private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display/msm_fb_ea_enable";
@@ -52,15 +45,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DozeUtils.checkDozeService(context);
         // Thermal Profiles
         ThermalUtils.startService(context);
-
-        //Micro-Service to restore sata of dt2w on reboot
-        SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE);
-        try {
-            mTouchFeature = ITouchFeature.getService();
-            mTouchFeature.setTouchMode(14,prefs.getInt(SHAREDD2TW, 1));
-        } catch (Exception e) {
-            // Do nothing
-        }
 
         // DC Dimming
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
