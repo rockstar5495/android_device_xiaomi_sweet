1423a0409a05ee6c84362c662af4dfd7c1b83fe6
neobuddy89 <neobuddy89@gmail.com>
2021-09-10 15:11:31 +0000

    sweet: Exclude mimoji zip files * For now, mimoji is non-functional. Let's exclude zip file to save some space.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -69,6 +69,8 @@ PRODUCT_COPY_FILES += \
    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/a2dp_audio_policy_configuration.xml \
    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/bluetooth_audio_policy_configuration.xml
 
+TARGET_DISABLE_MIMOJI_FILES := true
+
 # Device Settings
 PRODUCT_PACKAGES += \
     XiaomiParts
