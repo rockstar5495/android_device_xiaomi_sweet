7794ef6081012849a906a28bf11a7c76ee0ed2de
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-09-20 21:52:31 +0200

    sweet: drop dolby blobs  * doesn't seems to work
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -18,7 +18,3 @@ ro.surface_flinger.set_touch_timer_ms=750
 ro.surface_flinger.support_kernel_idle_timer=true
 ro.surface_flinger.set_display_power_timer_ms=1000
 ro.surface_flinger.use_content_detection_for_refresh_rate=true
-
-# Dolby
-ro.vendor.audio.dolby.dax.support=true
-ro.vendor.dolby.dax.version=DAX3_3.6.0.12_r1
