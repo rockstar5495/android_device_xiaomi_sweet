435724ba33bad062ed09889e8f1729addcbe410b
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-11-14 14:59:20 +0100

    sweet: adapt to Vantom Kernel
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,15 +54,25 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_sweet
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=1 loop.max_part=7
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := sweet_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := sweet_user_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
+
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+HOSTCFLAGS="-Wno-unused-command-line-argument"
+
+TARGET_KERNEL_APPEND_DTB := true
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
